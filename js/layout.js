390c4e67545bb8b07ca51a934de4e0b245c00a3f
miharacomclub <51486443+miharacomclub@users.noreply.github.com>
2019-12-26 14:26:35 +0900

    fix

--- js/layout.js
+++ js/layout.js
@@ -73,7 +73,7 @@ const updateLayout = () => {
 	$("#citation" ).textContent = `『${source[0]}』より作成`;
 	$("#dateTaken").parentNode.setAttributeNS("http://www.w3.org/1999/xlink", "href", photo.getAttribute("src"));
 	$("#citation" ).parentNode.setAttributeNS("http://www.w3.org/1999/xlink", "href", source[1]);
-	$$(".linkicon").forEach(icon => icon.setAttribute("x", $("#citation").getStartPositionOfChar(0).x - 15));
+	$$("#citation~*").forEach(icon => icon.setAttribute("x", $("#citation").getStartPositionOfChar(0).x - 19));
 };
 
 $("#yearSlider").addEventListener("input", updateLayout);
