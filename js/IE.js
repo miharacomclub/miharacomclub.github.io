ff97609902f63e31bf75c2a46ea847d62a1adabf
miharacomclub <51486443+miharacomclub@users.noreply.github.com>
2020-04-27 11:18:25 +0900

    ie

--- js/IE.js
+++ js/IE.js
@@ -1,4 +1,20 @@
-if (/trident/i.test(navigator.userAgent)) document.getElementsByTagName("h2")[0].outerHTML +=
-'<p style="font-weight:bold;background:lavender;padding:1em;">現在お使いのブラウザでは、一部機能がご利用になれません。<br>' +
-'<a target="_blank" rel="noopner" href="https://www.google.com/intl/ja_ALL/chrome/">Google Chrome</a>や、' +
-'<a target="_blank" rel="noopner" href="https://www.mozilla.org/ja/firefox/new/" class="nowrap">Firefox</a>などのブラウザをお使いください。</p>';
+var h2 = document.getElementsByTagName("h2")[0];
+
+if (/trident/i.test(navigator.userAgent)) {
+	h2.outerHTML +=
+		'<p class="warning">' +
+			'現在お使いのブラウザでは、一部機能がご利用になれません。<br>' +
+			'<a target="_blank" rel="noopner" href="https://www.google.com/intl/ja_ALL/chrome/">Google Chrome</a>や、' +
+			'<a target="_blank" rel="noopner" href="https://www.mozilla.org/ja/firefox/new/" class="nowrap">Firefox</a>などのブラウザをお使いください。' +
+		'</p>';
+}
+
+if (new Date() < new Date('2020/10/1')) {
+	if (location.search.indexOf("from=webcrow") > -1 || h2.textContent === 'トップページ') {
+		h2.outerHTML +=
+			'<p class="warning">' +
+				'<time datetime="2020-09-30">2020年9月30日</time>に、旧URL（http://miharacomb.webcrow.jp）から<br>' +
+				'現URL（https://miharacomclub.github.io）へのリダイレクトを終了します。' +
+			'</p>';
+	}
+}
