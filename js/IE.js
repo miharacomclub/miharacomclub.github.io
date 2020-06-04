34ea3da44b3126d6db9b6df4955f355075866c03
miharacomclub <51486443+miharacomclub@users.noreply.github.com>
2020-06-05 01:53:15 +0900

    vector-effect

--- js/IE.js
+++ js/IE.js
@@ -1,6 +1,6 @@
 var h2 = document.getElementsByTagName("h2")[0];
 
-if (/trident/i.test(navigator.userAgent)) {
+if (/trident/i.test(navigator.userAgent) || (!CSS.supports("(vector-effect: non-scaling-stroke)") && h2.textContent === '生徒数の推移')) {
 	h2.outerHTML +=
 		'<p class="warning">' +
 			'現在お使いのブラウザでは、一部機能がご利用になれません。<br>' +
